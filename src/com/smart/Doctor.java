d1100db83f22e7caf1d80a1bb003b90c33bf638b
Jon <jsmartware@aol.com>
2021-09-11 11:09:23 -0700

    refactored sample apps

---
+++ src/com/smart/Doctor.java
@@ -0,0 +1,17 @@
+package com.smart;
+
+public enum Doctor {
+   avery("Ralph Avery"),
+   johnson("Beth Johnson"),
+   murphy("Pat Murpy");
+
+   private String name;
+
+   Doctor(String name) {
+      this.name = name;
+   }
+
+   public String getName() {
+      return name;
+   }
+}
